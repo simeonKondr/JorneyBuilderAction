1eee72875e33e2ca38b1527ebb1375d3da46d31d
Simeon Kondrashev <simeon.kondrashev@hpe.com>
2022-07-14 00:12:46 +0400

    dfsf

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express();              //Instantiate an express app, the main work 
 const port = process.env.PORT || 5000;                  //Save the port number where your server will be listening
 
 //Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
+// app.get('/', (req, res) => {        //get requests to the root ("/") will route here
+//     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+//                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+// });
+app.use(express.static(__dirname));
+
+// app.use('/', __dirname);
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
